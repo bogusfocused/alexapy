6c7413510e7a18cbe6e853c9b099793ac9916dce
Alan Tse <alandtse@gmail.com>
2019-09-18 09:22:36 +0000

    Merge branch 'dev' into 'master'
    feat: add configflow support
    
    See merge request keatontaylor/alexapy!72
    
    (cherry picked from commit 82f8944cdfdae64385b8157dace4859898ffd0c6)
    
    0e1953b7 feat(alexalogin): add function to close open sessions
    7cfe6452 feat(alexalogin): allow changing of password through data input
    7a01e07c style(alexalogin): align rememberDevice submission with browsers
    6dc3adbe fix(alexalogin): fix bug where prior site ignored
    eb00b0f3 style: reorder logging for resp history
    de9408d0 style: add expiration data for cookie logs
    8278f5d3 fix(alexalogin): fix bug where cookies were not expired and caused login errors
    0a9b1fd5 chore: add bump to ci files
    6f3ec419 docs: correct README and add badge
    2657b568 Merge branch 'configflow' into 'dev'
    5d2be659 chore: update ci to install make
    3141484a Merge branch 'configflow' into 'dev'
    7d73f8f2 chore: remove homeassistant from pipfile
    a7d3f303 fix: fix cyclic import errors
    444e06b6 chore: add gcc for alpine build
    55efe7fd Merge branch 'configflow' into 'dev'
    a9c86b80 chore: add semantic-release for publishing
    c0ff9d52 chore: add git to ci script
    48231aa8 chore: remove pipenv from ci
    528df038 Merge branch 'configflow' into 'dev'

---
+++ alexapy/cookiejar.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  SPDX-License-Identifier: Apache-2.0
+"""
+Temporary fix of aiohttp/cookiejar until patch merged.
+
+https://github.com/aio-libs/aiohttp/pull/4066
+For more details about this api, please refer to the documentation at
+https://gitlab.com/keatontaylor/alexapy
+"""
+import datetime
+from http.cookies import Morsel, SimpleCookie  # noqa
+from typing import Mapping  # noqa
+
+from aiohttp.cookiejar import CookieJar
+from aiohttp.helpers import is_ip_address
+from aiohttp.typedefs import LooseCookies
+from yarl import URL
+
+
+class FixedCookieJar(CookieJar):
+    # pylint: disable=too-many-branches
+    """Fixed version of CookieJar that handles expired cookies."""
+
+    def update_cookies(self,
+                       cookies: LooseCookies,
+                       response_url: URL = URL()) -> None:
+        """Update cookies."""
+        hostname = response_url.raw_host
+
+        if not self._unsafe and is_ip_address(hostname):
+            # Don't accept cookies from IPs
+            return
+
+        if isinstance(cookies, Mapping):
+            cookies = cookies.items()  # type: ignore
+
+        for name, cookie in cookies:
+            if not isinstance(cookie, Morsel):
+                tmp = SimpleCookie()
+                tmp[name] = cookie  # type: ignore
+                cookie = tmp[name]
+
+            domain = cookie["domain"]
+
+            # ignore domains with trailing dots
+            if domain.endswith('.'):
+                domain = ""
+                del cookie["domain"]
+
+            if not domain and hostname is not None:
+                # Set the cookie's domain to the response hostname
+                # and set its host-only-flag
+                self._host_only_cookies.add((hostname, name))
+                domain = cookie["domain"] = hostname
+
+            if domain.startswith("."):
+                # Remove leading dot
+                domain = domain[1:]
+                cookie["domain"] = domain
+
+            if hostname and not self._is_domain_match(domain, hostname):
+                # Setting cookies for different domains is not allowed
+                continue
+
+            path = cookie["path"]
+            if not path or not path.startswith("/"):
+                # Set the cookie's path to the response path
+                path = response_url.path
+                if not path.startswith("/"):
+                    path = "/"
+                else:
+                    # Cut everything from the last slash to the end
+                    path = "/" + path[1:path.rfind("/")]
+                cookie["path"] = path
+
+            max_age = cookie["max-age"]
+            if max_age:
+                try:
+                    delta_seconds = int(max_age)
+                    self._expire_cookie(self._loop.time() + delta_seconds,
+                                        domain, name)
+                except ValueError:
+                    cookie["max-age"] = ""
+
+            else:
+                expires = cookie["expires"]
+                if expires:
+                    expire_time = self._parse_date(expires)
+                    if expire_time:
+                        diff = (expire_time.timestamp() -
+                                datetime.datetime.now().timestamp())
+                        self._expire_cookie(diff,
+                                            domain, name)
+                    else:
+                        cookie["expires"] = ""
+
+            self._cookies[domain][name] = cookie
+
+        self._do_expiration()
