602b8fae84a51c9928f79a27805954f332a76391
Alan D. Tse <alandtse@gmail.com>
2020-03-15 00:05:58 -0700

    fix: catch keyerrors as connection error

--- alexapy/helpers.py
+++ alexapy/helpers.py
@@ -67,7 +67,7 @@ def _catch_all_exceptions(func):
         template = "An exception of type {0} occurred." " Arguments:\n{1!r}"
         try:
             return func(*args, **kwargs)
-        except ClientConnectionError as ex:
+        except (ClientConnectionError, KeyError) as ex:
             message = template.format(type(ex).__name__, ex.args)
             _LOGGER.error(
                 "%s.%s: A connection error occured: %s",
